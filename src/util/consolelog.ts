c339419bea4e9a0db848c33bb2e16f9608533998
AxtTom <wolftom4321@gmail.com>
2022-04-22 20:20:18 +0200

    kp man

--- src/util/consolelog.ts
+++ src/util/consolelog.ts
@@ -23,17 +23,32 @@ function config() {
     }
     console.error = (x) => {
         const today = new Date();
-        fs.appendFileSync(
-            'logs/' + today.getFullYear().toString().padStart(4, '0') +
-            '-' + (today.getMonth()+1).toString().padStart(2, '0') +
-            '-' + today.getDate().toString().padStart(2, '0') +
-            '.err.log', 
-            '[' + today.getHours().toString().padStart(2, '0') + 
-            ":" + today.getMinutes().toString().padStart(2, '0') + 
-            ":" + today.getSeconds().toString().padStart(2, '0') + 
-            ']' + JSON.stringify(x) + 
-            '\n'
-        );
+        try {
+            fs.appendFileSync(
+                'logs/' + today.getFullYear().toString().padStart(4, '0') +
+                '-' + (today.getMonth()+1).toString().padStart(2, '0') +
+                '-' + today.getDate().toString().padStart(2, '0') +
+                '.err.log', 
+                '[' + today.getHours().toString().padStart(2, '0') + 
+                ":" + today.getMinutes().toString().padStart(2, '0') + 
+                ":" + today.getSeconds().toString().padStart(2, '0') + 
+                ']' + JSON.stringify(x) + 
+                '\n'
+            );
+        }
+        catch {
+            fs.appendFileSync(
+                'logs/' + today.getFullYear().toString().padStart(4, '0') +
+                '-' + (today.getMonth()+1).toString().padStart(2, '0') +
+                '-' + today.getDate().toString().padStart(2, '0') +
+                '.err.log', 
+                '[' + today.getHours().toString().padStart(2, '0') + 
+                ":" + today.getMinutes().toString().padStart(2, '0') + 
+                ":" + today.getSeconds().toString().padStart(2, '0') + 
+                ']' + x + 
+                '\n'
+            );
+        }
         consoleerr(x);
     }
 }
